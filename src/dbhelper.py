539e9c4a31fbd51a097f921b34f1f815dedbc2fc
Praveen Krishna <praveenkrishna0512@gmail.com>
2022-07-15 19:35:36 +0800

    uploaded base code, might change drastically

---
+++ src/dbhelper.py
@@ -0,0 +1,99 @@
+from operator import ne
+import sqlite3
+dayList = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+noSessionString = "No Session"
+thisWeekString = "This Week"
+nextWeekString = "Next Week"
+
+class DBHelper:
+    
+    def __init__(self, dbname="userData.sqlite"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname)
+
+    # Timeslot Availability is captured in 16 bits for each day
+    # First 8 bits represent this week, Last 8 bits represent next week
+    # 0 means not available, 1 means available
+    # The 8 bits represent these timeslots in this order: 7am, 9am, 11am, 1pm, 3pm, 5pm, 7pm, 9pm
+    def setup(self):
+        stmt = """CREATE TABLE IF NOT EXISTS userData ( 
+                        username TEXT PRIMARY KEY,
+                        inSession INTEGER DEFAULT 0,
+                        P
+                        )"""
+        self.conn.execute(stmt)
+        self.conn.commit()
+
+    # Username Queries
+
+    def handleUsername(self, username):
+        stmt = """SELECT * FROM userData WHERE username = (?)"""
+        args = (username, )
+        queryReturn = [x[0] for x in self.conn.execute(stmt, args)]
+        if len(queryReturn) == 0:
+            self.__addUsername(username)
+
+    def __addUsername(self, username):
+        stmt = "INSERT INTO userData (username) VALUES (?)"
+        args = (username, )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def getAllUsernames(self):
+        stmt = """SELECT username FROM userData"""
+        return [x[0] for x in self.conn.execute(stmt)]
+
+    # Session queries
+
+    def setSession(self, username, state):
+        # STATES (encoding - value passed in)
+        # 0 - not amending, 1 - this week, 2 - next week
+        inSession = self.determineSession(state)
+        stmt2 = """UPDATE userData SET inSession = (?) WHERE username = (?)"""
+        args2 = (inSession, username, )
+        self.conn.execute(stmt2, args2)
+        self.conn.commit()
+
+    def determineSession(self, state):
+        if state == noSessionString:
+            return 0
+        if state == thisWeekString:
+            return 1
+        if state == nextWeekString:
+            return 2
+        else:
+            print("ERROR in determineSession: Bad Week State Input")
+
+    def getSession(self, username):
+        currSession = self.getSessionHelper(username)
+        if currSession != thisWeekString and currSession != nextWeekString:
+            print("ERROR in getSession: Wrong Session Input!")
+            return
+        return currSession
+
+    def getSessionHelper(self, username):
+        stmt = """SELECT inSession FROM userData WHERE username = (?)"""
+        args = (username, )
+        state = -1
+        for x in self.conn.execute(stmt, args):
+            state = x[0]
+        if state == 0:
+            return noSessionString
+        if state == 1:
+            return thisWeekString
+        if state == 2:
+            return nextWeekString
+
+    # Get All User Data
+
+    def getAllUserData(self, username):
+        stmt = "SELECT * FROM userData WHERE username = (?)"
+        args = (username, )
+        return self.conn.execute(stmt, args)
+
+    # Purge data queries
+
+    def purgeUserData(self):
+        stmt = "DELETE FROM userData"
+        self.conn.execute(stmt)
+        self.conn.commit()
---
+++ src/main.py
@@ -0,0 +1,123 @@
+from calendar import weekday
+import logging
+import this
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from click import option
+from env import get_api_key, get_port
+import telebot
+from telebot import types
+import telegram
+from telegram import CallbackQuery, ParseMode
+import ast
+from dbhelper import DBHelper
+
+PORT = get_port()
+API_KEY = get_api_key()
+if API_KEY == None:
+    raise Exception("Please update API Key")
+
+# -----------------------------------------------------------------------------------------------------
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Initialise bot
+bot = telebot.TeleBot(API_KEY, parse_mode = None)
+
+# Makes Inline Keyboard
+# TODO Change callbakc_data values
+def makeInlineKeyboard(lst, optionID):
+    markup = types.InlineKeyboardMarkup()
+    for key, value in lst.items():
+        markup.add(types.InlineKeyboardButton(text = value,
+                                            callback_data = "['optionID', '" + optionID + "', 'value', '" + value + "']"))
+    return markup
+
+# Specific Inline Keyboard for showing time slots
+# TODO Change callbakc_data values
+def makeTimeInlineKeyboard(lst, optionID, dayPicked):
+    markup = types.InlineKeyboardMarkup()
+    for key, value in lst.items():
+        markup.add(types.InlineKeyboardButton(text = value,
+                                            callback_data = "['optionID', '" + optionID + "', 'value', '" + value + "', 'day', '" + dayPicked + "']"))
+    return markup
+
+# TODO: DB to main file converters
+
+
+# Command Handlers
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    txt1 = "Hi! Welcome to GymTogether\n\n"
+    txt2 = "Type <b>/help</b> for more info\n"
+    fullText = txt1 + txt2
+    update.message.reply_text(text = fullText, parse_mode = ParseMode.HTML)
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    txt1 = "Here are the suppported individual commands:\n"
+    txt2 = "<b>/schedule</b> - Begin scheduling your gym slots for this/next week\n\n"
+    txt3 = "Here are the support group commands:\n"
+    txt4 = "<b>/see</b> - See the current schedule"
+    fullText = txt1 + txt2 + txt3 + txt4
+    update.message.reply_text(text = fullText, parse_mode = ParseMode.HTML)
+
+def mainCallBackHandler(update, context):
+    dataClicked = ast.literal_eval(update.callback_query.data)
+    optionID = dataClicked[1]
+    value = dataClicked[3]
+    user = update.callback_query.message.chat.username
+
+    # Create database (this is required to ensure multiple ppl dont use the same db object)
+    db = DBHelper("userData.sqlite")
+    db.setup()
+    db.handleUsername(user)
+
+def stop(update, context):
+    """Stops"""
+    update.message.reply_text(update.message.text)
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    # Start the bot.
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(API_KEY, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # Handle all callback
+    dp.add_handler(CallbackQueryHandler(callback=mainCallBackHandler, pattern=str))
+
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    # updater.start_webhook(listen="0.0.0.0",
+    #                     port=int(PORT),
+    #                     url_path=str(API_KEY))
+    # updater.bot.setWebhook('https://whispering-dawn-13866.herokuapp.com/' + str(API_KEY))
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
